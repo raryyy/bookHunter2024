651b14b734cb845ec55210f534d264d290689f2e
RARY <ryan.senterre@gmail.com>
2024-09-01 16:10:00 +0200

    Route par défaut: Pages .home

--- app/config/params.php
+++ app/config/params.php
@@ -6,7 +6,7 @@ $title = "";
 $content = "";
 
 // PARAMÈTRES DE CONNEXION
-define("DB_HOST", "localhost:8889");
+define("DB_HOST", "localhost:3306");
 define("DB_NAME", "book_hunter_2024");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
