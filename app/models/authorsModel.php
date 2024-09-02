d73d4d2a404e95a4dbeb46f250bae50b7723731d
RARY <ryan.senterre@gmail.com>
2024-09-02 13:25:34 +0200

    route par défaut bis

--- app/models/authorsModel.php
+++ app/models/authorsModel.php
@@ -5,10 +5,10 @@ use \PDO;
 
 function findAll(PDO $connexion): array{
 
-    $sql = "SELECT *
-            FROM authors
-            ORDER BY created_at
-            Limit 3;";
+        $sql = "SELECT *
+                FROM authors
+                ORDER BY created_at DESC
+                Limit 3;";
 
     return $connexion->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
--- app/models/booksModel.php
+++ app/models/booksModel.php
@@ -7,7 +7,7 @@ function findAll(PDO $connexion): array{
 
     $sql = "SELECT *
             FROM books
-            ORDER BY created_at
+            ORDER BY created_at DESC
             Limit 3;";
 
     return $connexion->query($sql)->fetchAll(PDO::FETCH_ASSOC);
